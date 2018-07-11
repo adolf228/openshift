85a06cce160a8801653d83531b1ff5e36bb41f8b
Dimas <dimkabelkin1@gmail.com>
2018-07-11 09:19:33 +0300

    chat

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let app = require('express')();
+let io = require('socket.io')(3000);
 
-app.get('/', (req, res) => {
-	res.send('Привет, чувак!');
+io.on('connect', (socket) => {
+    io.emit('user connected', socket.id);
+    console.log('User ' + socket.id + ' connected');
+
+    socket.on('send message', (message) => {
+        var message = JSON.stringify({userName: socket.id, messageText: message});
+        io.emit('send message', message);
+        console.log(socket.id + ': ' + message);
+    });
+
+    socket.on('write message', (userId) => {
+        io.emit('write message', userId);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('user disconnected', socket.id);
+        console.log('User ' + socket.id + ' disconnected');
+    });
+
 });
-
-app.listen(8080);
